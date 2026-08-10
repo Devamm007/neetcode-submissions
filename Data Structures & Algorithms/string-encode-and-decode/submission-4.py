b49f4879721a712fad4a0eea01599def21b128d5
Devam Shah <143351720+Devamm007@users.noreply.github.com>
2026-08-10 16:35:04 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,65 @@
+class Solution:
+    # # Length Prefic Encoding Method
+    # def __init__(self):
+    #     # can be any character other than 0-9 digits
+    #     self.delimiter = ' '
+
+    # def encode(self, strs: List[str]) -> str:
+    #     encoded = []
+    #     for s in strs:
+    #         encoded.append(f"{len(s)}{self.delimiter}{s}")
+    #     return "".join(encoded)
+
+    # def decode(self, s: str) -> List[str]:
+    #     strs = []
+    #     i = 0
+    #     while i < len(s):
+    #         j = i
+    #         while s[j] != self.delimiter:
+    #             j+=1
+
+    #         length = int(s[i:j])
+
+    #         i = j+1
+    #         strs.append(s[i:i+length])
+
+    #         i += length
+    #     return strs
+
+    # Escaping Method
+    def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return "EMPTY"
+        encoded = []
+        for s in strs:
+            if not s:
+                continue
+            escaped_s = s.replace('\\', '\\\\').replace(',', '\\,')
+            encoded.append(escaped_s)
+        return 'LIST,'+','.join(encoded)
+
+    def decode(self, s: str) -> List[str]:
+        if s == 'EMPTY':
+            return []
+        else:
+            s = s[5:]
+        strs = []
+        current = []
+        i = 0
+        while i < len(s):
+            # escape
+            if s[i] == '\\' and i+1 < len(s):
+                current.append(s[i+1])
+                i+=2
+            # end
+            elif s[i] == ',':
+                strs.append("".join(current))
+                current = []
+                i+=1
+            # continue
+            else:
+                current.append(s[i])
+                i+=1
+        # last word
+        strs.append("".join(current))
+        return strs
